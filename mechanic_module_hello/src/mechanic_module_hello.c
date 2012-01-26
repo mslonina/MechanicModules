a09f7f888c833f0d977138dabede6c842ee5cb00
Mariusz Słonina <mariusz.slonina@gmail.com>
2012-01-26 02:05:26 +0100

    Small doc fixes

--- mechanic_module_hello/src/mechanic_module_hello.c
+++ mechanic_module_hello/src/mechanic_module_hello.c
@@ -1,45 +1,7 @@
-/*
- * HELLO -- SAMPLE MECHANIC MODULE
- * Copyright (c) 2010-2011, Mariusz Slonina (Nicolaus Copernicus University)
- * All rights reserved.
- *
- * This file is part of MECHANIC code.
- *
- * MECHANIC was created to help solving many numerical problems by providing
- * tools for improving scalability and functionality of the code. MECHANIC was
- * released in belief it will be useful. If you are going to use this code, or
- * its parts, please consider referring to the authors either by the website
- * or the user guide reference.
- *
- * http://git.astri.umk.pl/project/mechanic
- *
- * Redistribution and use in source and binary forms,
- * with or without modification, are permitted provided
- * that the following conditions are met:
- *
- * - Redistributions of source code must retain the above copyright notice,
- *   this list of conditions and the following disclaimer.
- * - Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- * - Neither the name of the Nicolaus Copernicus University nor the names of
- *   its contributors may be used to endorse or promote products derived from
- *   this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+/**
+ * @file
+ * The Hello, a sample Mechanic module
  */
-
-/* [HELLO] */
 
 /**
  * To understand what Mechanic is and what it does, let us write a well-known
@@ -199,15 +161,6 @@
  *
  */
 
-/* [/HELLO] */
-
-/**
- * @internal
- * Sample module for Mechanic
- * Keep is simple, but powerful!
- */
-
-/* [HELLOCODE] */
 #include "mechanic.h"
 #include "mechanic_module_hello.h"
 
@@ -286,5 +239,4 @@ int hello_worker_out(int nodes, int node, TaskInfo* md, TaskConfig* d,
   return MECHANIC_TASK_SUCCESS;
 }
 
-/* [/HELLOCODE] */
 
